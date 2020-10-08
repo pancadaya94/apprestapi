de2ddaff5697e5a45ceafcef8b1554fdb18d081d
unknown <pancadaya94@gmail.com>
2020-10-08 23:59:11 +0700

    membuat program CRUD dengan Node JS, api, Mysql

--- controller.js
+++ controller.js
@@ -49,3 +49,34 @@ exports.tambahMahasiswa = function(req,res) {
     })
 
 }
+
+//mengubah data berdasarkan id 
+exports.ubahMahasiswa = function(req,res){
+    var id = req.body.id_mahasiswa
+    var nim = req.body.nim
+    var nama = req.body.nama
+    var jurusan = req.body.jurusan
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?' , [nim,nama,jurusan,id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error)
+        }else{
+            response.ok('Berhasil ubah Data !!', res)
+        }
+    })
+}
+
+
+//Mengapus mahasiswa berdasarkan ID
+exports.hapusMahasiswa = function(req,res){
+    var id = req.body.id_mahasiswa
+    connection.query('DELETE from mahasiswa WHERE id_mahasiswa=?',[id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error)
+        }else {
+            response.ok('Berhasil menghapus mahasiswa !!',res)
+        }
+    })
+}
