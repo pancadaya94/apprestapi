5b6543d86c2a17e1ea7ed6445b29b6edd8fc5951
unknown <pancadaya94@gmail.com>
2020-10-08 15:08:24 +0700

    setting controller

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res')
 var connection = require('./koneksi')
 
 exports.index = function(req,res){
-    response.ok('aplikasi REST API ku berjalan!')
+    response.ok('aplikasi REST API ku berjalan!',res)
 }
