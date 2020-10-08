759179f15d98d009ccae8406c4eae7a9223a9739
unknown <pancadaya94@gmail.com>
2020-10-08 15:29:52 +0700

    berhasil menampilkan mahasiswa berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,17 @@ exports.tampilsemuamahasiswa = function(req,res){
         }       
     })
 }
+
+//menampilkan semua data mahasiswa berdasarkan ID
+exports.tampilberdasarkanid = function(req,res){
+  let id = req.params.id
+  connection.query('select * from mahasiswa where id_mahasiswa = ?', [id],
+    function(error, rows, fields) {
+        if(error){
+            console.log(error) 
+        }else {
+            response.ok(rows, res)
+        }
+    }
+  )   
+}
