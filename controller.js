8c3baa56e77f7d342d4282062669f5a53a5b9698
unknown <pancadaya94@gmail.com>
2020-10-08 15:24:19 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi')
 exports.index = function(req,res){
     response.ok('aplikasi REST API ku berjalan!',res)
 }
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('select * from mahasiswa', function(error, rows, fields){
+        if(error) {
+            connection.log(error)
+        }else {
+            response.ok(rows, res)
+        }       
+    })
+}
